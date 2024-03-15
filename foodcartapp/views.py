cff9b8ea51d8a559f7c24ec7a9fcc3205dcf1282
shadowsking <shadowsking1295@gmail.com>
2024-03-15 07:21:37 +0300

    Added transaction

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from django.http import JsonResponse
 from django.templatetags.static import static
 from rest_framework import status
@@ -69,6 +70,7 @@ def product_list_api(request):
     })
 
 
+@transaction.atomic
 @api_view(['POST'])
 def register_order(request):
     data = request.data
