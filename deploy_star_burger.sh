23af02f48072ec60753233887bb87b3ca8e1c34c
shadowsking <shadowsking1295@gmail.com>
2024-10-21 10:55:53 +0300

    Docker deploy script

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -3,20 +3,13 @@ cd /opt/star-burger
 git status
 git pull
 
-npm ci --dev
-./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
+#docker-compose -f .\docker-compose-dev.yml up --build
+#docker-compose up -d --force-recreate
+docker-compose up -d --build
 
-if [ ! -d "venv" ]; then
-    python -m venv venv
-fi
+docker exec star-burger-backend python manage.py collectstatic --noinput
+docker exec star-burger-backend python manage.py migrate --noinput
 
-source venv/bin/activate
-pip install -r requirements.txt
-
-python manage.py collectstatic --noinput
-python manage.py migrate --noinput
-
-sudo systemctl restart star-burger
 sudo systemctl reload nginx
 
 source .env
